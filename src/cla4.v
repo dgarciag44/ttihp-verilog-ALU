edfe56915f80deacfc23e21f7297f7361032bab9
dgarciag44 <143653115+dgarciag44@users.noreply.github.com>
2025-05-28 13:17:18 -0600

    Update cla4.v

--- src/cla4.v
+++ src/cla4.v
@@ -4,25 +4,23 @@ module cla4 (
     input  [3:0] A,
     input  [3:0] B,
     input        Cin,
-    output [3:0] Sum,
-    output       Cout,
-    output       P,    // Propagate de bloque
-    output       G     // Generate de bloque
+    output [3:0] S,
+    output       Cout
 );
-    wire [3:0] p, g;
-    wire [4:0] c;
 
-    assign p = A ^ B;
-    assign g = A & B;
-    assign c[0] = Cin;
-    assign c[1] = g[0] | (p[0] & c[0]);
-    assign c[2] = g[1] | (p[1] & c[1]);
-    assign c[3] = g[2] | (p[2] & c[2]);
-    assign c[4] = g[3] | (p[3] & c[3]);
+    wire [3:0] P, G;
+    wire [4:0] C;
 
-    assign Sum  = p ^ c[3:0];
-    assign Cout = c[4];
+    assign P = A ^ B;      // Propagate
+    assign G = A & B;      // Generate
 
-    assign P = &p;  // Group propagate (todos propagan)
-    assign G = g[3] | (p[3] & g[2]) | (p[3] & p[2] & g[1]) | (p[3] & p[2] & p[1] & g[0]);
+    assign C[0] = Cin;
+    assign C[1] = G[0] | (P[0] & C[0]);
+    assign C[2] = G[1] | (P[1] & C[1]);
+    assign C[3] = G[2] | (P[2] & C[2]);
+    assign C[4] = G[3] | (P[3] & C[3]);
+
+    assign S = P ^ C[3:0]; // Final sum
+    assign Cout = C[4];    // Final carry out
+
 endmodule
